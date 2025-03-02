806d7df0e85a23005954c6cb5a5927b1d2852f1d
jwliao-ai <jwliao.ai@gmail.com>
2025-03-02 15:12:32 +0800

    test(math_dual): added test .sh file.

--- fctncalling_rft/scripts/math_dual_tppo_3B.sh
+++ fctncalling_rft/scripts/math_dual_tppo_3B.sh
@@ -13,11 +13,11 @@ python train_math.py \
         --lr 1e-8 \
         --critic_lr 5e-7 \
         --dataset_path ../envs/math/data/merged_precalculus_train.json \
-        --model_name_or_path /ext0/hcchai/codemate/Qwen2.5-Coder-1.5B-Instruct/ \
+        --model_name_or_path /ext0/hcchai/codemate/Qwen2.5-Coder-3B-Instruct/ \
         --n_agents 2 \
         --agent_iteration_interval 3000 \
         --profile_path profiles/math_dual.json \
-        --n_rollout_threads 2 \
+        --n_rollout_threads 1 \
         --episode_length 3 \
         --gradient_cp_steps 4 \
         --context_window 2048 \
